61a8c5cc75ffdf798d30cb21b673d7cef30d76a7
Hari <hariom987316@gmail.com>
2021-04-15 23:48:08 +0530

    Code Check-in

--- Angular-TF-JS/src/app/app.component.ts
+++ Angular-TF-JS/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+// Import model
+import { PredictionConfig } from '../model/prediction-config';
+import { PredictionResult } from '../model/prediction-result';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'Angular-TF-JS';
+  title = 'Hariom Kumar Angular-TF-JS';
+
+  data: PredictionConfig = {
+    objectToDetect: 'person',
+    threshold: 0.7,
+    quitOnFound: false,
+    showConfettiWhenFound: true
+  };
+
+
+  // Prediction Result
+  Predictionresults: PredictionResult[] = [];
+  handlePredictionChange(Predictionresults: PredictionResult[]): void {
+    this.Predictionresults = Predictionresults;
+  }
 }
--- Angular-TF-JS/src/app/app.module.ts
+++ Angular-TF-JS/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TFPredictionComponent } from './tf-prediction/tf-prediction.component';
 
+// Import installed module
+import { WebcamModule } from 'ngx-webcam';
+
 @NgModule({
   declarations: [
     AppComponent,
     TFPredictionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
