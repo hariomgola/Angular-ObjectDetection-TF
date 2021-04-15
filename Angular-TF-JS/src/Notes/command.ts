fada1ecd5144d6e9f02acf93c8bd38ac8284f389
Hari <hariom987316@gmail.com>
2021-04-15 23:01:47 +0530

    Notes checkin

---
+++ Angular-TF-JS/src/Notes/command.ts
@@ -0,0 +1,30 @@
+/**
+
+
+ -- Install ngx-webcam command
+                - npm install --save ngx-webcam
+
+ -- Intall tensorflow/tfjs command
+                - npm install @tensorflow/tfjs --save
+
+ -- Intall tensorflow-models/coco-ssd command
+                - npm install @tensorflow-models/coco-ssd --save
+
+ -- Intall canvas-confetti command
+                - npm install --save canvas-confetti
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
